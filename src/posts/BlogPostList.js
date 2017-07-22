6b7f65645449a44378dd0da043dc190080686416
elchrisso <elchrisso@hotmail.com>
2017-07-22 07:15:34 -0600

    graph.cool-hosted posts are now displaying in BlogPostList.

--- src/posts/BlogPostList.js
+++ src/posts/BlogPostList.js
@@ -4,16 +4,33 @@ import { graphql } from 'react-apollo'
 import { Table, Button } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
+import BlogEntryPreview from './BlogEntryPreview'
+import { fetchAllPosts } from '../graphql/posts.graph'
+
 
 
 class BlogPostList extends Component {
+
   render () {
+
+    let posts = null
+    if (this.props.data.allPosts) {
+      posts = this.props.data.allPosts//.map((post) => {
+        return (
+          <BlogEntryPreview blogEntryList={posts}/>
+        )
+      //})
+    }
+
     return (
       <h1>
-        blogpostList
+        {posts}
       </h1>
     )
   }
 }
 
-export default BlogPostList
+const withPostQuery = graphql(fetchAllPosts, {options: {
+  fetchPolicy: 'network-only' }})(BlogPostList)
+
+export default withPostQuery
